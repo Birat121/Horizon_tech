f06b0e731676a5dda3d10e194260afa8c259bc17
Birat Budhathoki <biratbudhathoki79@gmail.com>
2024-12-24 16:21:58 +0545

    sidebar added

--- frontend/inventory/src/App.jsx
+++ frontend/inventory/src/App.jsx
@@ -1,37 +1,43 @@
+import { Route, Routes } from "react-router-dom";
 
-import { Route, Routes } from 'react-router-dom'
-
-import Navbar from './components/Navbar/Navbar'
-import Master from './components/Master/Master'
-import Transaction from './components/Transaction/Transaction'
-import AccountReports from './components/Account Reports/AccountReports'
-import Home from './pages/Home'
-import Exit from './components/Exit/Exit'
-import InventoryReports from './components/Inventory Reports/InventoryReports'
-import SystemSecurity from './components/System Security/SystemSecurity'
-import About from './components/About Software/About'
+import Sidebar from "./components/Sidebar/Sidebar";
+import Master from "./components/Master/Master";
+import Transaction from "./components/Transaction/Transaction";
+import AccountReports from "./components/Account Reports/AccountReports";
+import Home from "./pages/Home";
+import Exit from "./components/Exit/Exit";
+import InventoryReports from "./components/Inventory Reports/InventoryReports";
+import SystemSecurity from "./components/System Security/SystemSecurity";
+import About from "./components/About Software/About";
 
 
 function App() {
-
-
   return (
-    <>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/master" element={<Master/>}/>
-      <Route path="/transaction" element={<Transaction/>}/>
-      <Route path="/accountreports" element={<AccountReports/>}/>
-      <Route path="/inventoryreports" element={<InventoryReports/>}/>
-      <Route path = "/systemsecurity" element={<SystemSecurity/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/exit" element={<Exit/>}/>
-    </Routes>
-    
+    <div className='bg-gray-50 min-h-screen'>
+      {/* Sidebar */}
+      
+      
+      <hr/>
+      <div className="flex w-full">
+        <Sidebar/>
+        <div className='w-[70%] mx-auto ml-[max(5vw,25px] my-8 text-gray-600 text-base'>
 
-      </>
-  )
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/master" element={<Master />} />
+          <Route path="/transaction" element={<Transaction />} />
+          <Route path="/accountreports" element={<AccountReports />} />
+          <Route path="/inventoryreports" element={<InventoryReports />} />
+          <Route path="/systemsecurity" element={<SystemSecurity />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/exit" element={<Exit />} />
+        </Routes>
+      </div>
+    </div>
+    
+    </div>
+  );
+
 }
 
-export default App
+export default App;
---
+++ frontend/inventory/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "font-awesome/css/font-awesome.min.css";
+import { menus } from "../../reusable inputs/menus";
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false); // Toggle sidebar visibility
+  const [activeDropdown, setActiveDropdown] = useState(null); // Track active dropdown menu
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const toggleDropdown = (menuName) => {
+    setActiveDropdown(activeDropdown === menuName ? null : menuName);
+  };
+
+  return (
+    <div className="flex h-screen">
+      {/* Sidebar Container */}
+      <div
+        className={`fixed inset-y-0 left-0 bg-white text-black w-64 transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 lg:translate-x-0`}
+      >
+        {/* Sidebar Header */}
+        <div className="p-6 flex justify-between items-center border-b border-zinc-500">
+          <h1 className="text-3xl font-bold">Horizon Tech</h1>
+          <button
+            onClick={toggleSidebar}
+            className="lg:hidden text-black text-2xl focus:outline-none"
+          >
+            <i className="fa fa-times"></i>
+          </button>
+        </div>
+
+        {/* Sidebar Links */}
+        <ul className="mt-4 space-y-2">
+          {menus.map((menu) => (
+            <li key={menu.name} className="relative group">
+              <button
+                onClick={() => toggleDropdown(menu.name)}
+                className="w-full text-left px-4 py-2 hover:bg-yellow-300 transition-colors"
+              >
+                {menu.name}
+              </button>
+
+              {/* Dropdown Menu */}
+              {activeDropdown === menu.name && (
+                <ul className="ml-4 mt-2 space-y-1">
+                  {menu.items.map((item, index) => (
+                    <li key={index} className="relative group">
+                      <Link
+                        to={item.path}
+                        className="block px-4 py-2 hover:bg-yellow-300"
+                      >
+                        {item.label}
+                      </Link>
+
+                      {/* Nested Sub-options */}
+                      {item.subOptions && (
+                        <ul className="ml-4 mt-1 space-y-1">
+                          {item.subOptions.map((subItem, subIndex) => (
+                            <li key={subIndex}>
+                              <Link
+                                to={subItem.path}
+                                className="block px-4 py-2 hover:bg-blue-500"
+                              >
+                                {subItem.label}
+                              </Link>
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+
+          {/* Static Links */}
+          <li>
+            <Link
+              to="/about"
+              className="block px-4 py-2 hover:bg-yellow-300 transition-colors"
+            >
+              About Software
+            </Link>
+          </li>
+          <li>
+            <button className="block w-full text-left px-4 py-2 hover:bg-yellow-300">
+              Exit
+            </button>
+          </li>
+        </ul>
+      </div>
+
+      {/* Hamburger Menu */}
+      <div className="lg:hidden p-4">
+        <button
+          onClick={toggleSidebar}
+          className="text-blue-900 text-2xl focus:outline-none"
+        >
+          <i className={`fa ${isOpen ? "fa-times" : "fa-bars"}`}></i>
+        </button>
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default Sidebar;
