05e719dc36c3ec906cf64d62b551ea52ce7a6e73
Sunil Ranabhat <mrsunilranabhat@gmail.com>
2024-12-25 16:42:47 +0545

    Formatting

--- frontend/inventory/src/App.jsx
+++ frontend/inventory/src/App.jsx
@@ -1,30 +1,29 @@
 import { Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Home from "./pages/Home";
+import Exit from "./components/Exit/Exit";
+import About from "./components/About Software/About";
+
+{/* Master Options */}
 import CreateAccountLedger from "./components/Master/CreateAccountLedger";
 import SubLedgerMaster from "./components/Master/SubLedgerMaster";
-<<<<<<< HEAD
-
-{/* Inventory Reports */}
-import InventoryItemList from "./components/Inventory Reports/InventoryItemList";
-import ItemSearch from "./components/Inventory Reports/ItemSearch";
-
-{/*System Security*/}
-import MenuAccess from "./components/System Security/MenuAccess";
-
-
-=======
 import AdditionalExpensesLedger from "./components/Master/AdditionalExpensesLedger";
 import AccountReGrouping from "./components/Master/AccountReGrouping";
 import DepartmentMaster from "./components/Master/DepartmentMaster";
 import CounterSetting from "./components/Master/CounterSetting";
 import PointCollectionSetting from "./components/Master/PointCollectionSetting";
->>>>>>> c2619ddb636a6057cd73e100c9e07ef1c4d6d28f
-import Home from "./pages/Home";
-import Exit from "./components/Exit/Exit";
-import About from "./components/About Software/About";
 import ChangeAccountType from "./components/Master/ChangeAccountType";
+{/* Master Options */}
+
+{/* Inventory Reports */}
+import InventoryItemList from "./components/Inventory Reports/InventoryItemList";
+{/* Inventory Reports */}
+
+{/* System Security */}
 import ChangePassword from "./components/System Security/ChangePassword";
 import SignUpUser from "./components/System Security/SignUpUser";
+import MenuAccess from "./components/System Security/MenuAccess";
+{/* System Security */}
 
 
 function App() {
@@ -35,35 +34,30 @@ function App() {
         <Sidebar/>
         <div className='w-[70%] mx-auto ml-[max(5vw,25px] my-8 text-gray-600 text-base'>
         <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/exit" element={<Exit />} />
 
           {/*Master Options*/}
-          <Route path="/" element={<Home />} />
           <Route path="/master/suboption1" element={<CreateAccountLedger />} />
           <Route path="/master/suboption2" element={<SubLedgerMaster />} />
-<<<<<<< HEAD
-
-          <Route path="/inventoryreports/suboption1" element={<ItemSearch />} />
-          <Route path="/inventoryreports/suboption2" element={<InventoryItemList/>} />
-
-          <Route path="/systemsecurity/suboptions1" element={<MenuAccess />} />
-=======
           <Route path="/master/suboption3" element={<AdditionalExpensesLedger />} />
           <Route path="/master/suboption4" element={<ChangeAccountType />} />
           <Route path="/master/suboption5" element={<AccountReGrouping/>} />
           <Route path="/master/suboption7" element={<DepartmentMaster />} />
           <Route path="/master/suboption8" element={<CounterSetting />} />
           <Route path="/master/suboption9" element={<PointCollectionSetting />} />
+          {/*Master Options*/}
 
-
+          {/*Inventory Reports */}
+          <Route path="/inventoryreports/suboption2" element={<InventoryItemList />} />
+          {/*Inventory Reports */}
 
           {/*System Security */}
+          <Route path="/systemsecurity/suboptions1" element={<MenuAccess />} />
           <Route path="/systemsecurity/suboptions3" element={<ChangePassword/>} />
           <Route path="/systemsecurity/suboptions4" element={<SignUpUser />} />
-
-
->>>>>>> c2619ddb636a6057cd73e100c9e07ef1c4d6d28f
-          <Route path="/about" element={<About />} />
-          <Route path="/exit" element={<Exit />} />
+          {/*System Security */}
         </Routes>
       </div>
     </div>
