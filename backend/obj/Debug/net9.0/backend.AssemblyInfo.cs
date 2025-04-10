f7979a75abb127e43e5ab7109a95fa7c5c55890e
rosham07 <roshambajgain7@gmail.com>
2025-04-10 13:52:03 +0545

    backend changes

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd8c79d89ae61fb6820e4be811a0fb38d2ab68e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b799c99abc2e3419050624f6fe205d828ef59c2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
