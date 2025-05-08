50efbe858e168b5a47b5cb2530dbb74cf2cad082
Birat Budhathoki <biratbudhathoki79@gmail.com>
2025-05-08 22:33:02 +0545

    basic changes

--- frontend/inventory/src/reusable inputs/config.js
+++ frontend/inventory/src/reusable inputs/config.js
@@ -9,6 +9,34 @@ import StockLocationMaster from "../components/Master/InventoryMaster/StockLocat
 const API_BASE_URL ="http://localhost:5143/api"; // Replace with your API base URL
 
 export const API_URLS = {
-  SUBCATEGORY: `${API_BASE_URL}/SubCategory`,
-  // Add other URLs here if needed in future
+  SUBCATEGORY: `${API_BASE_URL}/SubCategory`,  
+  SAVE_COUNTER_SETTING: `${API_BASE_URL}/CounterSetting/createCount`,
+ 
+  CreateDepartment: `${API_BASE_URL}/Department/CreateDepartment`,
+  UpdateDepartment: `${API_BASE_URL}/Department/UpdateDepartment`,
+
+  CreateUOM :  `${API_BASE_URL}/UOM/CreateUom`, 
+  UpdateUOM : `${API_BASE_URL}/UOM/UpdateUom`,
+
+
+  CreateCategory : `${API_BASE_URL}/Category/CreateCategory`,
+  UpdateCategory : `${API_BASE_URL}/Category/UpdateCategory`,
+
+  CreateServiceItem : `${API_BASE_URL}/ServiceItemMaster/CreateServiceItem`,
+  UpdateServiceItem : `${API_BASE_URL}/ServiceItemMaster/UpdateServiceItem`,
+
+
+  CreateStockLoc : `${API_BASE_URL}/StockLocationMaster/CreateStockLoc`,
+  UpdateStockLoc : `${API_BASE_URL}/StockLocationMaster/UpdateStockLoc`,
+
+  //system security
+
+
+
+  
 };
+
+
+
+  SUBCATEGORY: `${API_BASE_URL}/SubCategory`
+  // Add other URLs here if needed in future
